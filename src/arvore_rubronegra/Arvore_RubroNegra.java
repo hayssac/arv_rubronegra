d85d04eadb65663a2f44b8d68f8a4b6cbb56cbef
Adrianny <a.ingridreis@gmail.com>
2017-05-17 21:04:25 -0300

    implementacao de remocao situação 1, 2 e 4.

--- src/arvore_rubronegra/Arvore_RubroNegra.java
+++ src/arvore_rubronegra/Arvore_RubroNegra.java
@@ -144,10 +144,6 @@ public class Arvore_RubroNegra {
          
     }
     
-    private void recolorir(No inserido){
-        
-    }
-    
     private void rotacaoSimplesDireita(No inserido){ // Caso 3a da inserção
         //out.println("rotação simples direita");
         No avo = inserido.getPai().getPai();
@@ -237,6 +233,199 @@ public class Arvore_RubroNegra {
         
         rotacaoSimplesEsquerda(pai);
     }
+    
+    private void remover(No no, int chave){
+        
+        //out.println(no.getChave());
+        
+        if(no.getChave() != chave){
+            if(no.getChave() < chave){
+                remover(no.getFilho_Direito(), chave);
+            } else {
+                remover(no.getFilho_Esquerdo(), chave);
+            }
+        } else { // ENCONTROU O NÓ A SER REMOVIDO
+            if(no.getFilho_Direito() == nil){
+                if(no.getFilho_Esquerdo() == nil){
+                    if(no.getCor()=="red"){
+                        if(no.getPai().getFilho_Direito()==no){
+                            no.getPai().setFilho_Direito(nil);
+                        } else {
+                            no.getPai().setFilho_Esquerdo(nil);
+                        }
+                    }else{
+                        nil.setDuplo(true);
+                        situacao3(no, nil);
+                    }
+                } else { // nó só tem filho esquerdo.
+                    if(no.getCor() == "red" && no.getFilho_Esquerdo().getCor() == "red"){
+                        situacao1(no, no.getFilho_Esquerdo());
+                    }else if(no.getCor() == "black" && no.getFilho_Esquerdo().getCor() == "red"){
+                        situacao2(no, no.getFilho_Esquerdo());
+                    } else if(no.getCor() == "black" && no.getFilho_Esquerdo().getCor() == "black"){
+                        no.getFilho_Esquerdo().setDuplo(true);
+                        situacao3(no, no.getFilho_Esquerdo());
+                    } else {
+                        situacao4(no, no.getFilho_Esquerdo());
+                    }
+                }
+            } else {
+                No filhoDireito = no.getFilho_Direito();
+            
+                while(filhoDireito.getFilho_Esquerdo()!=nil){
+                    filhoDireito = filhoDireito.getFilho_Esquerdo();
+                }
+                
+                if(no.getCor() == "red" && filhoDireito.getCor() == "red"){
+                    situacao1(no, filhoDireito);
+                }else if(no.getCor() == "black" && filhoDireito.getCor() == "red"){
+                    situacao2(no, filhoDireito);
+                } else if(no.getCor() == "black" && filhoDireito.getCor() == "black"){
+                    filhoDireito.setDuplo(true);
+                    situacao3(no, filhoDireito);
+                } else {
+                    situacao4(no, filhoDireito);
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    private void situacao1(No removido, No sucessor){
+        if(removido.getPai().getFilho_Esquerdo()==removido){
+            if(sucessor.getPai().getFilho_Direito() == sucessor){
+                sucessor.getPai().setFilho_Direito(nil);
+            } else {
+                sucessor.getPai().setFilho_Esquerdo(nil);
+            }
+            
+            removido.getPai().setFilho_Esquerdo(sucessor);
+            removido.getFilho_Direito().setPai(sucessor);
+            removido.getFilho_Esquerdo().setPai(sucessor);
+            sucessor.setPai(removido.getPai());
+            sucessor.setFilho_Direito(removido.getFilho_Direito());
+            sucessor.setFilho_Esquerdo(removido.getFilho_Esquerdo());
+        }else {
+            if(sucessor.getPai().getFilho_Esquerdo()== sucessor){
+                sucessor.getPai().setFilho_Esquerdo(nil);
+            } else {
+                sucessor.getPai().setFilho_Direito(nil);
+            }
+            
+            removido.getPai().setFilho_Direito(sucessor);
+            removido.getFilho_Esquerdo().setPai(sucessor);
+            removido.getFilho_Direito().setPai(sucessor);
+            sucessor.setPai(removido.getPai());
+            sucessor.setFilho_Esquerdo(removido.getFilho_Esquerdo());
+            sucessor.setFilho_Direito(removido.getFilho_Direito());
+        }
+    }
+    
+    private void situacao2(No removido, No sucessor){
+        if(removido.getPai().getFilho_Esquerdo()==removido){
+            if(sucessor.getPai().getFilho_Direito() == sucessor){
+                sucessor.getPai().setFilho_Direito(nil);
+            } else {
+                sucessor.getPai().setFilho_Esquerdo(nil);
+            }
+            
+            if(removido.getPai() == null){
+                noRaiz = sucessor;
+            }
+            
+            removido.getPai().setFilho_Esquerdo(sucessor);
+            removido.getFilho_Direito().setPai(sucessor);
+            removido.getFilho_Esquerdo().setPai(sucessor);
+            sucessor.setPai(removido.getPai());
+            sucessor.setFilho_Direito(removido.getFilho_Direito());
+            sucessor.setFilho_Esquerdo(removido.getFilho_Esquerdo());
+            
+            sucessor.setCor("black");
+        } else {
+            if(sucessor.getPai().getFilho_Esquerdo()== sucessor){
+                sucessor.getPai().setFilho_Esquerdo(nil);
+            } else {
+                sucessor.getPai().setFilho_Direito(nil);
+            }
+            
+            if(removido.getPai() == null){
+                noRaiz = sucessor;
+            }
+            
+            removido.getPai().setFilho_Direito(sucessor);
+            removido.getFilho_Esquerdo().setPai(sucessor);
+            removido.getFilho_Direito().setPai(sucessor);
+            sucessor.setPai(removido.getPai());
+            sucessor.setFilho_Esquerdo(removido.getFilho_Esquerdo());
+            sucessor.setFilho_Direito(removido.getFilho_Direito());
+            
+            sucessor.setCor("black");
+        }
+    }
+    
+    private void situacao3(No removido, No sucessor){
+        //out.println("entrou na situação 3");
+        
+        No irmao;
+        No pai = removido.getPai();
+        
+        if(pai.getFilho_Direito() == removido){
+            irmao = pai.getFilho_Esquerdo();
+        } else {
+            irmao = pai.getFilho_Direito();
+        }
+        
+        //caso 1
+        if(sucessor.getCor()=="black" && irmao.getCor()=="red" && pai.getCor()=="black"){
+            //situacao3_1(pai, irmao, sucessor);
+        }
+    }
+    
+    public void situacao3_1(){
+        
+    }
+    
+    public void situacao3_2a(){
+        
+    }
+    
+    public void situacao3_2b(No sucessor){
+    }
+    
+    public void situacao3_3(){
+        
+    }
+    
+    public void situacao3_4(){
+        
+        
+    }
+    
+    private void situacao4(No removido, No sucessor){
+        //out.println(sucessor.getChave() +" "+ sucessor.getCor());
+        if(removido.getPai().getFilho_Esquerdo()==removido){
+            if(sucessor.getPai().getFilho_Direito() == sucessor){
+                sucessor.getPai().setFilho_Direito(nil);
+            } else {
+                sucessor.getPai().setFilho_Esquerdo(nil);
+            }
+            
+            removido.getPai().setFilho_Esquerdo(sucessor);
+            removido.getFilho_Direito().setPai(sucessor);
+            removido.getFilho_Esquerdo().setPai(sucessor);
+            sucessor.setPai(removido.getPai());
+            sucessor.setFilho_Direito(removido.getFilho_Direito());
+            sucessor.setFilho_Esquerdo(removido.getFilho_Esquerdo());
+            
+           
+            sucessor.setCor("red");
+            sucessor.setDuplo(true);
+        }
+        
+        situacao3(removido, sucessor);
+    }
 
     public static void emOrdem(No raiz) {
         if(raiz != null) {
@@ -251,6 +440,19 @@ public class Arvore_RubroNegra {
     
     public static void main(String[] args) {
         Arvore_RubroNegra aRN = new Arvore_RubroNegra();
+        
+        aRN.inserir(aRN.getRaiz(), 7);
+        aRN.inserir(aRN.getRaiz(), 2);
+        aRN.inserir(aRN.getRaiz(), 10);
+        aRN.inserir(aRN.getRaiz(), 1);
+        aRN.inserir(aRN.getRaiz(), 8);
+        aRN.inserir(aRN.getRaiz(), 5);
+        aRN.inserir(aRN.getRaiz(), 15);
+        aRN.inserir(aRN.getRaiz(), 13);
+        
+        aRN.remover(aRN.getRaiz(), 10);
+        
+        
         
         // TESTES DO PROFESSOR
         
